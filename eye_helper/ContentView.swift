3fae67f7ee5caa66b87a9bcd83f82004f5b92409
Sam Liu <amazing_sam@outlook.com>
2025-03-31 18:22:21 +0800

    Initial Commit

---
+++ eye_helper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eye_helper
+//
+//  Created by 刘一笔 on 2025/3/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eye_helper/eye_helperApp.swift
@@ -0,0 +1,17 @@
+//
+//  eye_helperApp.swift
+//  eye_helper
+//
+//  Created by 刘一笔 on 2025/3/31.
+//
+
+import SwiftUI
+
+@main
+struct eye_helperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
